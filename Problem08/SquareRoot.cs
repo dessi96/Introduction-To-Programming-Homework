2b64d9be58c25bfacd2c01daf6ba92ef8ab10eac
dessi96 <dessislava96@gmail.com>
2015-06-28 16:02:10 +0300

    Introduction to Programming, Homework
    Visual Studio, C#

---
+++ Problem08/SquareRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+            int num = 12345;
+            Console.WriteLine(Math.Sqrt(num));
+        }
+    }
+
---
+++ Problem15/AgeAfterTenYears.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class AgeAfterTenYears
+    {
+        static void Main()
+        {
+            Console.Write("Now: ");
+            int ageOld = int.Parse(Console.ReadLine());
+            Console.WriteLine("After 10 years: " + (ageOld + 10));
+        }
+    }
+
